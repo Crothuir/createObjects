dd8d808d817acd52fba31266f02fff3538b70412
Conor Murphy <conorpaulmurphy@hotmail.com>
2019-07-02 12:39:47 +0100

    Basic setup

---
+++ CreateObjectDemo.java
@@ -0,0 +1,17 @@
+package createObject;
+
+public class CreateObjectDemo{
+	
+	public static void main(String[] args){
+		
+		// Declare and create a point object and two rectangle objects.
+		
+		// Display rectOne's width, height, and area.
+		
+		// Set rectTwo's position
+		
+		// Display rectTwo's position
+		
+		// Move rectTwo and display its new position
+	}
+}
